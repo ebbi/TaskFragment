9fe4c8029a40e9a86f8791e31b7e8150b108a8a1
Ebbi <ebbi40@gmail.com>
2019-12-27 13:46:29 +0000

    ViewModel used to communicate between dynamically loaded fragments

--- app/src/main/java/com/example/taskfragment/MainActivity.java
+++ app/src/main/java/com/example/taskfragment/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.taskfragment.ui.main.TaskFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, TaskFragment.newInstance())
+                    .commitNow();
+        }
+
     }
 }
